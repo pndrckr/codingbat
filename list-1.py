b1f52b79bd8c2b104c5e1f4d2971643896fe2768
Tati <pndrckr@users.noreply.github.com>
2017-12-01 19:27:39 -0300

    Update list-1.py

--- list-1.py
+++ list-1.py
@@ -39,3 +39,31 @@ def same_first_last(nums):
 def make_pi():
   pi = [3, 1, 4]
   return pi
+
+
+
+############### common_end
+
+# Dados 2 arrays de entero, a y b, devuelve True si
+# coinciden el primer o ultimo elemento
+
+# common_end([1, 2, 3], [7, 3]) → True
+# common_end([1, 2, 3], [7, 3, 2]) → False
+# common_end([1, 2, 3], [1, 3]) → True
+
+def common_end(a, b):
+  return a[0] == b[0] or a[-1] == b[-1]
+
+
+
+############### sum3 
+
+# Dado un array de numeros enteros de largo 3
+# devuelve la suma de los elementos
+
+# sum3([1, 2, 3]) → 6
+# sum3([5, 11, 2]) → 18
+# sum3([7, 0, 0]) → 7
+
+def sum3(nums):
+  return nums[0]+nums[1]+nums[2]
